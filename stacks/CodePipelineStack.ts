6a957edca9bc5537a8d38a3c93e57c07415665b0
Kuda WillZ <kudawillz@hotmail.com>
2022-02-03 22:16:59 +0000

    Update CodePipelineStack.ts

--- stacks/CodePipelineStack.ts
+++ stacks/CodePipelineStack.ts
@@ -4,7 +4,11 @@ import { Construct } from 'constructs';
 export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
-
+      
+        // Creates a CodeCommit repository called 'WorkshopRepo'
+        new codecommit.Repository(this, 'TestRepo', {
+            repositoryName: "TestRepo"
+        });
         // Pipeline code goes here
     }
 }
