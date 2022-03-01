18060941dde804b1061989e97bb28fffbe703d9d
jamlen <code@james-allen.co.uk>
2022-03-01 11:47:10 +0000

    BRD-200 Minor change to force rebuild

--- src/triggers/postAuth.ts
+++ src/triggers/postAuth.ts
@@ -1,28 +1,13 @@
 import { PutItemInput } from "aws-sdk/clients/dynamodb"
 import dynamoDb from "../util/dynamodb"
-// import { ActoraApiClient } from '../clients/actoraApiClient'
 import { EventBridge } from "aws-sdk"
 
 const client = new EventBridge()
-// const actoraApi = ActoraApiClient.create()
 
 export async function main(event: any) {
     const accountId = event.requestContext?.authorizer.iam.cognitoIdentity.identityId
-    console.log('postAuth trigger', event, accountId)
     const data = event.request.userAttributes
-    // // actually want to publish an event to say they have auth'd
-    // // then a subscriber would fire off to ACT to create the customer
-    // // and then update the cognito record with the externalRef
-
-    // const api = await actoraApi
-    // const { externalRef } = await api.customerRegister({
-    //   forename: data.given_name,
-    //   surname: data.family_name,
-    // })
-    // // const card = await actoraApi.cardRequest({
-    // //   cardType: 'adult',
-    // //   card
-    // // })
+    console.log('postAuth trigger', event, data, accountId)
     const params = {
         TableName: process.env.CUSTOMERS_TABLE_NAME,
         Item: {
